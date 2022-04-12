523f1797cb9b86a59d395d0fe46fa1f78be7b8ba
Jose Sandoval <joseasandovalp@gmail.com>
2022-04-12 23:04:40 +0200

    Add bottom nav to main menu

--- app/src/main/java/com/gori/acmeexplorer/MainMenuActivity.java
+++ app/src/main/java/com/gori/acmeexplorer/MainMenuActivity.java
@@ -1,17 +1,22 @@
 package com.gori.acmeexplorer;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ListView;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.gori.acmeexplorer.adapters.MainMenuAdapter;
+import com.gori.acmeexplorer.auth.LoginActivity;
 import com.gori.acmeexplorer.models.MenuItem;
 
 import java.util.ArrayList;
 
-public class MainMenuActivity extends AppCompatActivity {
+public class MainMenuActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
     private ArrayList<MenuItem> menuItems;
+    private BottomNavigationView bottomNav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +28,21 @@ public class MainMenuActivity extends AppCompatActivity {
         MainMenuAdapter adapter = new MainMenuAdapter(menuItems, this);
 
         ListView lvMainMenu = findViewById(R.id.lvMainMenu);
+        bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(this);
 
         lvMainMenu.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull android.view.MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.page_profile:
+                Intent intent = new Intent(this, LoginActivity.class);
+                startActivity(intent);
+                return true;
+        }
+
+        return false;
     }
 }
--- app/src/main/java/com/gori/acmeexplorer/auth/LoginActivity.java
+++ app/src/main/java/com/gori/acmeexplorer/auth/LoginActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.gori.acmeexplorer.R;
+
 public class LoginActivity extends AppCompatActivity {
 
     @Override
