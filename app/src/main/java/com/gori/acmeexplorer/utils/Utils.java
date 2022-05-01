1c048f3cdf0c98b134b622857e65652e88c30d7e
Jose Sandoval <joseasandovalp@gmail.com>
2022-05-01 13:54:40 +0200

    chore: remove unused code

--- app/src/main/java/com/gori/acmeexplorer/utils/Utils.java
+++ app/src/main/java/com/gori/acmeexplorer/utils/Utils.java
@@ -1,36 +1,10 @@
 package com.gori.acmeexplorer.utils;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonDeserializationContext;
-import com.google.gson.JsonDeserializer;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParseException;
-import com.google.gson.JsonPrimitive;
-import com.google.gson.JsonSerializationContext;
-import com.google.gson.JsonSerializer;
-import com.google.gson.reflect.TypeToken;
-import com.gori.acmeexplorer.models.Trip;
-
-import java.lang.reflect.Type;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.time.format.FormatStyle;
-import java.util.ArrayList;
 import java.util.Date;
 
 public class Utils {
-    public static String SHARED_DATA_UNIQUE_NAME = "com.gori.acmeexplorer";
-    public static String SHARED_DATA_TRIPS = "trips-data";
-    public static String SHARED_DATA_SELECTED_TRIPS = "selected-trip-data";
-
-    public static Type tripArrayType = new TypeToken<ArrayList<Trip>>() {
-    }.getType();
-
-    public static DateTimeFormatter dateFormatter = DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT);
-
     public static Date parseDate(String date) {
         try {
             return new SimpleDateFormat("dd-MM-yyyy").parse(date);
@@ -43,26 +17,7 @@ public class Utils {
         return new SimpleDateFormat().format(date);
     }
 
-
     public static String twoDigits(int n) {
         return (n < 10) ? ("0" + n) : String.valueOf(n);
     }
-
-
-    public static Gson gson = new GsonBuilder()
-            .setPrettyPrinting()
-            .registerTypeAdapter(LocalDate.class, new LocalDateConverter())
-            .create();
-
-    public static class LocalDateConverter implements JsonSerializer<LocalDate>, JsonDeserializer<LocalDate> {
-        public JsonElement serialize(LocalDate src, Type typeOfSrc, JsonSerializationContext context) {
-            return new JsonPrimitive(DateTimeFormatter.ISO_LOCAL_DATE.format(src));
-        }
-
-        public LocalDate deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
-                throws JsonParseException {
-            return DateTimeFormatter.ISO_LOCAL_DATE.parse(json.getAsString(), LocalDate::from);
-        }
-    }
-
 }
